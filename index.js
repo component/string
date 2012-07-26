37e6894b4067bad5947319d90087a525e6f6475b
TJ Holowaychuk <tj@vision-media.ca>
2012-07-26 16:28:35 -0700

    add .capitalizeWords() variant

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ exports.capitalize = function(str){
 exports.uncapitalize = function(str){
   return str.charAt(0).toLowerCase() + str.slice(1);
 };
+
+exports.capitalizeWords = function(str){
+  return str.replace(/\w+/g, function(word){
+    return exports.capitalize(word);
+  });
+};
+
+exports.uncapitalizeWords = function(str){
+  return str.replace(/\w+/g, function(word){
+    return exports.uncapitalize(word);
+  });
+};
--- test/string.js
+++ test/string.js
@@ -3,12 +3,25 @@ var _ = require('..');
 
 describe('.capitalize(str)', function(){
   it('should capitalize the first letter', function(){
-    _.capitalize('hey').should.equal('Hey');
+    _.capitalize('hey there').should.equal('Hey there');
   })
 })
 
 describe('.uncapitalize(str)', function(){
   it('should uncapitalize the first letter', function(){
-    _.uncapitalize('Hey').should.equal('hey');
+    _.uncapitalize('Hey there').should.equal('hey there');
+  })
+})
+
+describe('.capitalizeWords(str)', function(){
+  it('should capitalize each word', function(){
+    _.capitalizeWords('hey there').should.equal('Hey There');
+  })
+})
+
+describe('.uncapitalizeWords(str)', function(){
+  it('should uncapitalize each word', function(){
+    _.uncapitalizeWords('Hey There').should.equal('hey there');
+    _.uncapitalizeWords('HeY TherE').should.equal('heY therE');
   })
 })
